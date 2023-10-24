0b2340114c50382a3b7a0647cac1bf52c94262d2
Lelcaren <lelcaren11@gmail.com>
2023-10-24 07:34:38 -0700

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,35 +1,23 @@
 #include "main.h"
 
 /**
-  * _strspn - main function
+  * _strchr - main function
   *
   * @s: Function parameter
   *
-  * @accept: Function parameter
+  * @c: Function parameter
   *
-  * Return: Always 0.
+  * Return: Always o.
   */
-unsigned int _strspn(char *s, char *accept)
+char *_strchr(char *s, char c)
 {
-
-	unsigned int _strspn(char *s, char *accept);
-	unsigned int i = 0;
 	int k;
 
-	while (*s)
+	for (k = 0; s[k] >= '\0'; k++)
 	{
-	for (k = 0; accept[k]; k++)
-	{
-	if (*s == accept[k])
-	{
-	i++;
-	break;
+	if (s[k] == c)
+	return (s + k);
 	}
-	else if (accept[k + 1] == '\0')
-	return (i);
-	}
-	s++;
-	}
-	return (i);
+	return (0);
 }
 
